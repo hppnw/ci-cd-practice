ba0fd2e8ccdde8d88d65a31c8337306725b3313d
hppnw <706489782@qq.com>
2025-12-05 11:54:32 +0800

    选项2-bug修复

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Cloud Computing Class!');
 });
